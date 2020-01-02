cf585c37adcfdc64ffe06d5a4e5bd12ed3d88abf
Anna Albuquerque <anna.b.albuquerque@gmail.com>
2020-01-02 18:16:08 -0300

    Answer changing according to the Question

--- lib/main.dart
+++ lib/main.dart
@@ -35,8 +35,18 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     var questions = [
-      "What's your favorite color ?",
-      "What's your favorite animal ?",
+      {
+        'questionText': 'What\'s your favorite color ?',
+        'answer': ['Black', 'Red', 'Green', 'White'],
+      },
+      {
+        'questionText': 'What\'s your favorite animal ?',
+        'answer': ['Rabbit', 'Snake', 'Elephant', 'Lion'],
+      },
+      {
+        'questionText': 'Who\'s your favorite instructor?',
+        'answer': ['Max', 'Max', 'Max', 'Max'],
+      },
     ];
     return MaterialApp(
       home: Scaffold(
@@ -45,11 +55,14 @@ class _MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: <Widget>[
-            Question(questionText: questions[_questionIndex]),
-            //We are passing the _answerQuestion function and this is called callback, since the receiving widget calls this function in the future
-            Answer(_answerQuestion),
-            Answer(_answerQuestion),
-            Answer(_answerQuestion),
+            Question(
+              questionText: questions[_questionIndex]['questionText'],
+            ),
+            // ... - take a list and pull all the values in the list out of it and add them to the surrounding list as individual values.
+            //With this instead of adding a list to a list we are adding the values of a list to a list
+            ...(questions[_questionIndex]['answer'] as List<String>).map((answer){
+              return Answer(_answerQuestion, answer);
+            }).toList()
           ],
         ),
       ),
